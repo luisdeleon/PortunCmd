4bb475167dcde72334eb9ddeb71235150fa52313
Luis DeLeon <luis.deleon@ebluenet.com>
2025-11-18 08:35:53 -0800

    Chore: Update auto-imports type definitions
    Update auto-generated type definitions to include usePropertyImport composable.

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -288,6 +288,7 @@ declare global {
   const usePreferredReducedMotion: typeof import('@vueuse/core')['usePreferredReducedMotion']
   const usePrevious: typeof import('@vueuse/core')['usePrevious']
   const useProjection: typeof import('@vueuse/math')['useProjection']
+  const usePropertyImport: typeof import('./src/composables/usePropertyImport')['usePropertyImport']
   const useRafFn: typeof import('@vueuse/core')['useRafFn']
   const useRefHistory: typeof import('@vueuse/core')['useRefHistory']
   const useResizeObserver: typeof import('@vueuse/core')['useResizeObserver']
@@ -656,6 +657,7 @@ declare module 'vue' {
     readonly usePreferredReducedMotion: UnwrapRef<typeof import('@vueuse/core')['usePreferredReducedMotion']>
     readonly usePrevious: UnwrapRef<typeof import('@vueuse/core')['usePrevious']>
     readonly useProjection: UnwrapRef<typeof import('@vueuse/math')['useProjection']>
+    readonly usePropertyImport: UnwrapRef<typeof import('./src/composables/usePropertyImport')['usePropertyImport']>
     readonly useRafFn: UnwrapRef<typeof import('@vueuse/core')['useRafFn']>
     readonly useRefHistory: UnwrapRef<typeof import('@vueuse/core')['useRefHistory']>
     readonly useResizeObserver: UnwrapRef<typeof import('@vueuse/core')['useResizeObserver']>
