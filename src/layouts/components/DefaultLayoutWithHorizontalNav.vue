071b7e5ec05fc61524c088e39794e51c6e362105
Luis DeLeon <luis.deleon@ebluenet.com>
2025-12-13 19:06:24 -0600

    Remove Shortcuts menu from navbar

--- src/layouts/components/DefaultLayoutWithHorizontalNav.vue
+++ src/layouts/components/DefaultLayoutWithHorizontalNav.vue
@@ -7,7 +7,6 @@ import { themeConfig } from '@themeConfig'
 import Footer from '@/layouts/components/Footer.vue'
 import NavBarNotifications from '@/layouts/components/NavBarNotifications.vue'
 import NavSearchBar from '@/layouts/components/NavSearchBar.vue'
-import NavbarShortcuts from '@/layouts/components/NavbarShortcuts.vue'
 import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
 import UserProfile from '@/layouts/components/UserProfile.vue'
 import NavBarI18n from '@core/components/I18n.vue'
@@ -39,7 +38,6 @@ import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
       />
 
       <NavbarThemeSwitcher />
-      <NavbarShortcuts />
       <NavBarNotifications class="me-2" />
       <UserProfile />
     </template>
--- src/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ src/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -6,7 +6,6 @@ import { themeConfig } from '@themeConfig'
 import Footer from '@/layouts/components/Footer.vue'
 import NavBarNotifications from '@/layouts/components/NavBarNotifications.vue'
 import NavSearchBar from '@/layouts/components/NavSearchBar.vue'
-import NavbarShortcuts from '@/layouts/components/NavbarShortcuts.vue'
 import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
 import UserProfile from '@/layouts/components/UserProfile.vue'
 import NavBarI18n from '@core/components/I18n.vue'
@@ -40,7 +39,6 @@ import { VerticalNavLayout } from '@layouts'
           :languages="themeConfig.app.i18n.langConfig"
         />
         <NavbarThemeSwitcher />
-        <NavbarShortcuts />
         <NavBarNotifications class="me-1" />
         <UserProfile />
       </div>
